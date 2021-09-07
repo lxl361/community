2b0b604b6a5b38b82114aa17291a5a54cf285d34
luxinlong <xinlong.lu@zd-automotive.cn>
2021-09-07 22:17:07 +0800

    first repo

---
+++ src/main/java/com/zd/communitity/CommunitityApplication.java
@@ -0,0 +1,13 @@
+package com.zd.communitity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zd/communitity/CommunitityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zd.communitity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
