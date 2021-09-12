8968158a4865283330b683e71ad8301eac03555f
luxinlong <xinlong.lu@zd-automotive.cn>
2021-09-12 10:29:27 +0800

    add h2 database

--- src/main/java/com/zd/communitity/controller/AuthorizationController.java
+++ src/main/java/com/zd/communitity/controller/AuthorizationController.java
@@ -2,12 +2,18 @@ package com.zd.communitity.controller;
 
 import com.zd.communitity.dto.AccessTokenDTO;
 import com.zd.communitity.dto.GithubUser;
+import com.zd.communitity.mapper.UserMapper;
+import com.zd.communitity.model.User;
 import com.zd.communitity.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizationController {
@@ -19,9 +25,12 @@ public class AuthorizationController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("callback")
     public String callback(@RequestParam(name ="code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -29,8 +38,21 @@ public class AuthorizationController {
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubuser = githubProvider.getUser(accessToken);
+        if (githubuser!=null){
+            //登录成功，写入session和cookie
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubuser.getName());
+            user.setAccountId(String.valueOf(githubuser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubuser);
+            return "redirect:/";
+        }else {
+            //登录失败，返回首页
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/zd/communitity/pojo/User.java
+++
@@ -1,7 +0,0 @@
-package com.zd.communitity.pojo;
-
-import java.io.Serializable;
-
-public class User implements Serializable {
-
-}
